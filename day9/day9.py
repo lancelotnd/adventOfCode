d89ceec7e381f45a8a7d4fb8740a9f081a27ce27
lancelotnd <lancelotnormand@outlook.com>
2021-12-10 13:35:18 -0500

    day 9 done

--- day9/day9.py
+++ day9/day9.py
@@ -1,34 +1,64 @@
 with open('input.txt') as f:
     lines = f.readlines()
 
+def getConnexIndex(hsize,  index, array):
+    toReturn = []
+    if (index % hsize) != 0:
+        toReturn.append(index-1)
+    if (index - hsize) >= 0:
+        toReturn.append(index-hsize)
+    if (index + hsize) < len(array):
+        toReturn.append(index+ hzise)
+    if (index + 1) % hsize  != 0:
+        toReturn.append(index+1)
+    return toReturn
 
 def getConnex(hsize,  index, array):
     toReturn = []
-    if index % hsize != 0:
+    if (index % hsize) != 0:
         toReturn.append(array[index-1])
-    if index - hsize >= 0:
+    if (index - hsize) >= 0:
         toReturn.append(array[index-hsize])
-    if index + hsize < len(array):
+    if (index + hsize) < len(array):
         toReturn.append(array[index+ hzise])
-    if index + 1 % hsize  != 0:
+    if (index + 1) % hsize  != 0:
         toReturn.append(array[index+ 1])
     return toReturn
 
+def findBassine(initialIndex, hsize, array):
+    visited = set()
+    unvisited = {initialIndex}
+    while(len(unvisited) != 0):
+        connexe = []
+        to_visit = unvisited.pop()
+        visited.add(to_visit)
+        connexe = getConnexIndex(hsize, to_visit, array)
 
-m = [int(l.replace('\n', '')) for l in lines]    
+        connexe = [x for x in connexe if x not in visited]
+        connexe = [x for x in connexe if array[x] != 9]
+        for e in connexe:
+            unvisited.add(e)
+    return list(visited)
+m = [l.replace('\n', '') for l in lines]    
 hzise = len(str(m[0]))
 vsize = len(m)
+
 master = []
 for n in m:
     tmp  = [x for x in str(n)]
     for i in tmp:
         master.append(int(i))
-
 mins = []
-
-for n in range(0, len(master)-1):
-    if master[n] < min(getConnex(hzise, n, master)):
+indexMin = []
+for n in range(len(master)):
+    connexe = getConnex(hzise, n, master)
+    if master[n] < min(connexe):
+        indexMin.append(n)
         mins.append(master[n])
-
-
-print("answer",sum([x+1 for x in mins]))
+totalSizes= []
+for i in indexMin:
+    visited = findBassine(i, hzise, master)
+    totalSizes.append(len(visited))
+totalSizes.sort(reverse=True)
+final_list = totalSizes[0:3]
+print("Answer is :",final_list[0] * final_list[1] * final_list[2])
